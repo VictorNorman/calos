5cbaab2b3edaf63cf916b4535d9798a105642ec9
Victor Norman <vtn2@calvin.edu>
2020-05-21 12:50:38 -0400

    add documentation to RAM class

--- ram.py
+++ ram.py
@@ -2,6 +2,10 @@ RAM_SIZE = 1024
 
 
 class RAM:
+    '''A representation of RAM. You can access it by using indexing operators: [].
+    These call __getitem__ and __setitem__ below. Note that this memory holds whatever
+    you put into it -- integers, floats, strings, etc. Each is stored in a single location.
+    '''
     def __init__(self, size=RAM_SIZE):
         self._minAddr = 0
         self._maxAddr = RAM_SIZE - 1
@@ -10,11 +14,14 @@ class RAM:
             self._memory.append(0)
 
     def __getitem__(self, addr):
+        '''called when a ram object is indexed/subscripted: ram[3], e.g.'''
+        assert self.is_legal_addr(addr)
         return self._memory[addr]
 
     def __setitem__(self, addr, val):
+        '''called when a ram object is indexed/subscripted: ram[3] = 44, e.g.'''
+        assert self.is_legal_addr(addr)
         self._memory[addr] = val
 
     def is_legal_addr(self, addr):
         return self._minAddr <= addr <= self._maxAddr
-
